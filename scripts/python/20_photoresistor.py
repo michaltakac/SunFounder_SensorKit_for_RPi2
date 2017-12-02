e8cacbd820acb028d2ab56e8c421f9728b0783b9
Michal Takac <takacmichal92@gmail.com>
2017-12-03 00:43:33 +0100

    play and pause

--- scripts/python/20_photoresistor.py
+++ scripts/python/20_photoresistor.py
@@ -16,7 +16,16 @@ def setup():
 
 def loop(ws):
   status = 1
-  while True:
+  isPlaying = True
+
+  ws.on_message(message):
+    if message == 'pause':
+      isPlaying = False
+
+    if message == 'play':
+      isPlaying = False
+
+  while isPlaying:
     print 'Value: ', ADC.read(0)
     ws.write_message(str(ADC.read(0)))
     time.sleep(0.2)
@@ -44,6 +53,5 @@ if __name__ == '__main__':
     setup()
     app.listen(8888)
     ioloop.IOLoop.instance().start()
-    #loop()
   except KeyboardInterrupt:
     pass
