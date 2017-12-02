5786c67f40ba9b3af64888a4be447a71e8650303
Michal Takac <takacmichal92@gmail.com>
2017-12-02 22:36:25 +0100

    add tornado server and websockets

--- scripts/python/20_photoresistor.py
+++ scripts/python/20_photoresistor.py
@@ -2,9 +2,24 @@
 import PCF8591 as ADC
 import RPi.GPIO as GPIO
 import time
+from tornado import websocket, web, ioloop
+import json
 
 DO = 17
 GPIO.setmode(GPIO.BCM)
+cl = []
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
 
 def setup():
 	ADC.setup(0x48)
@@ -15,12 +30,18 @@ def loop():
 	status = 1
 	while True:
 		print 'Value: ', ADC.read(0)
-		
+
 		time.sleep(0.2)
+
+app = web.Application([
+    (r'/ws', SocketHandler),
+])
 
 if __name__ == '__main__':
 	try:
 		setup()
 		loop()
-	except KeyboardInterrupt: 
-		pass	
+    app.listen(8888)
+    ioloop.IOLoop.instance().start()
+	except KeyboardInterrupt:
+		pass
