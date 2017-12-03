17e38af1f289e33782a9d522cec2e1d0f5aaa7d0
Michal Takac <takacmichal92@gmail.com>
2017-12-03 11:07:15 +0100

    fix port and remove old stuff

--- scripts/python/20_photoresistor.py
+++ scripts/python/20_photoresistor.py
@@ -7,11 +7,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
 import json
 
-define("port", default=8888, help="run on the given port", type=int)
-
 DO = 17
 GPIO.setmode(GPIO.BCM)
-cl = []
 is_playing = False
 
 app = Flask(__name__)
@@ -28,26 +25,6 @@ def loop():
         emit('sensor_data', {'data': str(ADC.read(0))})
         time.sleep(0.2)
 
-class SocketHandler(websocket.WebSocketHandler):
-  is_playing = True
-
-  def check_origin(self, origin):
-    return True
-
-  def open(self):
-    if self not in cl:
-      cl.append(self)
-      while SocketHandler.is_playing:
-        print 'Value: ', ADC.read(0)
-        self.write_message(str(ADC.read(0)))
-        time.sleep(0.2)
-
-  def on_close(self):
-    if self in cl:
-        cl.remove(self)
-        SocketHandler.is_playing = False
-
-
 @socketio.on('sensor_start')
 def handle_start_event():
     is_playing = True
@@ -59,8 +36,8 @@ def handle_stop_event():
 def main():
     setup()
     loop()
-    socketio.run(app)
-    print "Server listening at http://localhost:", options.port
+    socketio.run(app, host='localhost', port=8888)
+    print "Server listening at http://localhost:8888"
 
 if __name__ == '__main__':
     main()
