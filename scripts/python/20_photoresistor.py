517b99555a9be3df04b352b5bf81b8b5244807ce
Michal Takac <takacmichal92@gmail.com>
2017-12-03 11:02:30 +0100

    try to use Flask and flask-socketio

--- scripts/python/20_photoresistor.py
+++ scripts/python/20_photoresistor.py
@@ -3,33 +3,30 @@ import logging
 import PCF8591 as ADC
 import RPi.GPIO as GPIO
 import time
-from tornado import websocket, web, ioloop
-import tornado.options
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, send
 import json
-
-from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
 
 DO = 17
 GPIO.setmode(GPIO.BCM)
 cl = []
+is_playing = False
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 def setup():
   ADC.setup(0x48)
   GPIO.setup(DO, GPIO.IN)
 
-
-class Application(web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/ws-photoresistor", SocketHandler),
-        ]
-        settings = dict(
-            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
-            xsrf_cookies=True,
-        )
-        super(Application, self).__init__(handlers, **settings)
+def loop():
+    while is_playing:
+        print 'Value: ', ADC.read(0)
+        emit('sensor_data', {'data': str(ADC.read(0))})
+        time.sleep(0.2)
 
 class SocketHandler(websocket.WebSocketHandler):
   is_playing = True
@@ -51,13 +48,19 @@ class SocketHandler(websocket.WebSocketHandler):
         SocketHandler.is_playing = False
 
 
+@socketio.on('sensor_start')
+def handle_start_event():
+    is_playing = True
+
+@socketio.on('sensor_stop')
+def handle_stop_event():
+    is_playing = False
+
 def main():
     setup()
-    tornado.options.parse_command_line()
-    app = Application()
-    app.listen(options.port)
+    loop()
+    socketio.run(app)
     print "Server listening at http://localhost:", options.port
-    ioloop.IOLoop.current().start()
 
 if __name__ == '__main__':
     main()
