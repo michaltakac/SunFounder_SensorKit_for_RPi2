6afd177d5544c5159c7c45187b572b947fa9d60a
Michal Takac <takacmichal92@gmail.com>
2017-12-03 01:37:10 +0100

    Application class, main function updates

--- scripts/python/20_photoresistor.py
+++ scripts/python/20_photoresistor.py
@@ -4,7 +4,12 @@ import PCF8591 as ADC
 import RPi.GPIO as GPIO
 import time
 from tornado import websocket, web, ioloop
+import tornado.options
 import json
+
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
 
 DO = 17
 GPIO.setmode(GPIO.BCM)
@@ -22,6 +27,19 @@ def loop(ws):
     print 'Value: ', ADC.read(0)
     ws.write_message(str(ADC.read(0)))
     time.sleep(0.2)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/ws-photoresistor", SocketHandler),
+        ]
+        settings = dict(
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            xsrf_cookies=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
 
 class SocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
@@ -44,12 +62,13 @@ class SocketHandler(websocket.WebSocketHandler):
       if message == 'play':
         isPlaying = True
 
-app = web.Application([
-    (r'/ws-photoresistor', SocketHandler),
-])
 
+def main():
+    setup()
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    ioloop.IOLoop.current().start()
 
 if __name__ == '__main__':
-  setup()
-  app.listen(8888)
-  ioloop.IOLoop.instance().start()
+    main()
